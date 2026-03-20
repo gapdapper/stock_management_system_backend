670f3269284e1197844ecfe964c85251e43495a3
Gap <puri.pongsomboonsuk@gmail.com>
2026-03-20 10:08:14 +0700

    feat: adjust sameSite: 'strict' + adjust CORS origin

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
 };
 
